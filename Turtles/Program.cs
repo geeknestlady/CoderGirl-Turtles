26f044209370274ad41ae846832b2c9dc953fc12
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-22 15:14:56 -0600

    Finished Homework

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -8,6 +8,11 @@ namespace Turtles
         {
             // TODO: Prints "I like turtles." 1000 times.
 
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("I like turtles.");
+            }
+
             Console.ReadLine();
         }
     }
